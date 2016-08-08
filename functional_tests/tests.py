98ca5addaf58f7599962cacafa9b198c919322c2
David Voong <voong.david@gmail.com>
2016-08-08 21:47:25 +0100

    Update functional tests to test the homepage title

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,5 +2,12 @@ from .functional_test import FunctionalTest
 
 class FunctionalTest(FunctionalTest):
 
-    def test2(self):
-        assert False, 'test'
+    def test(self):
+        url = self.live_server_url
+        self.browser.get(url)
+        title = self.browser.title
+        self.assertEqual(title, '''Lin's Alterations''')
+
+        # Prices
+        # Location
+        # Contact
